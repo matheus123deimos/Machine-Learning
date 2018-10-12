c55840d764a44cbbdbe4c3bc3bee3a07e21adfdc
unknown <matheusgomescord@gmail.com>
2018-10-11 23:47:10 -0300

    Mod_Script_1

--- Homework_2/parte_0/Script.r
+++ Homework_2/parte_0/Script.r
@@ -1,5 +1,7 @@
 library(e1071)
 library(ggplot2)
+library(corrplot)
+
 
 train_y = read.table("solTrainY.txt")
 train_x = read.table("solTrainX.txt")
@@ -11,6 +13,7 @@ Data = rbind(train_x, test_x)
 Data_trans = rbind(trainx_trans, testx_trans)
 Dados = Data_trans
 Dados["y"] = rbind(train_y,test_y)
+Cor = cor(Data_trans[209:228])
 
 tabela = function(a){
   k = c(1)
@@ -27,10 +30,28 @@ tabela = function(a){
   return(df)
 }
 
-for(i in 1:2){
-  ggplot(Dados,aes(Dados[,i])) + geom_histogram(aes(fill=y))+theme_gray()+
-  labs(x=names(Dados[i]),y="Frequência")
-  ggsave(paste(names(Dados[i]),".png"))
+#Plotagem da Matriz de Correlação somente para as 16 ultimas características
+png("Matriz_Correlação_N.png")
+corrplot(Cor,type="upper",method="circle")
+dev.off()
+
+#Plotagem de todos os histogramas de Dispersão
+for(i in 1:228){
+  ggplot(Data_trans,aes(Data_trans[,i])) + geom_histogram(color="black",fill="springgreen2")+theme_gray()+
+  labs(x=names(Data_trans[i]),y="Frequência")
+  ggsave(paste(names(Data_trans[i]),".png"))
   dev.off()
 }
 
+#Plotagem Dispersão
+for(i in 209:228){
+  for(j in 209:228){
+    if(i>j){
+      ggplot(Dados,aes(Data_trans[,j],Dados[,i]))+ geom_point(aes(color=y)) +labs(x=names(Data_trans[i]),y=names(Data_trans[j]),colour="outcome")
+      ggsave(paste(names(Dados[i])," vs ",names(Dados[j]),".png"))
+      dev.off()
+    }
+  }
+}
+
+print(tabela(Data_trans))
